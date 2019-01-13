e202628841438c014afbc65684dc3fa28bcabc37
eric-klimowich <eklimowich@hotmail.com>
2019-01-13 12:40:15 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+
+  handleFocusClick = () => {
+    console.log('Good!')
+  }
+
+  handleBlurClick = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    return (
+      <button
+        onFocus={this.handleFocusClick}
+        onBlur={this.handleBlurClick}
+      >
+        EyesOnMe
+      </button>
+    )
+  }
+
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+
+  handleChange = () => console.log('Entering password...')
+
+  render() {
+    return <input type="password" onKeyUp={this.handleChange} />
+  }
+  
+}
+
+export default Keypad;
